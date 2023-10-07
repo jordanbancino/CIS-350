cf5d36f2d139890ab0369a294ee3257d2ca7c46c
Jordan Bancino <jordan@bancino.net>
2023-10-07 09:07:18 -0400

    Reformat arg.py
    This makes the warnings go away.

--- src/arg.py
+++ src/arg.py
@@ -8,19 +8,20 @@ import arithmetic
 
 fps = 120  # frames per second
 
+
 def main() -> None:
     """
-	The game entry function. All initial setup and final teardown
-	happens here; this function is always the first pushed on the
-	stack and that last popped off, at which point the program
-	exits.
-	"""
+    The game entry function. All initial setup and final teardown
+    happens here; this function is always the first pushed on the
+    stack and that last popped off, at which point the program
+    exits.
+    """
     clock = pygame.time.Clock()  # keep track of time
     set_main_menu(window)
     # start_game()
     running = True
     while running:
-        clock.tick(fps) # run while-loop at a max rate of fps
+        clock.tick(fps)  # run while-loop at a max rate of fps
         if start_button.draw(window):
             update_ui(window)
         if quit_button.draw(window):
@@ -29,8 +30,6 @@ def main() -> None:
             if event.type == pygame.QUIT:  # if user pressed window's "X"
                 running = False
                 pygame.quit()
-
-    
 
 
 if __name__ == "__main__":
